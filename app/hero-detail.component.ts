89bdcd159e4f2982fb41c80cf721197f562a5b5a
Facundo Flores <flores.facundogabriel@gmail.com>
2016-08-29 14:54:42 -0300

    Update and save

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,33 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
-import { Hero } from './hero';
+import { Hero }        from './hero';
 import { HeroService } from './hero.service';
-
 @Component({
   selector: 'my-hero-detail',
   templateUrl: 'app/hero-detail.component.html',
   styleUrls: ['app/hero-detail.component.css']
 })
-
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
   constructor(
     private heroService: HeroService,
-    private route: ActivatedRoute
-  ) {
-
+    private route: ActivatedRoute) {
   }
-  @Input()
-  hero: Hero;
-
   ngOnInit(): void {
     this.route.params.forEach((params: Params) => {
-      let id = +params['id']; // Convert route parameter to number with + JS
-      this.heroService.getHero(id).then(hero => this.hero = hero);
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
     });
   }
-  goBack(): void {
-    window.history.back(); // Navigates backward one step in browser his stack
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(this.goBack);
   }
-
+  goBack(): void {
+    window.history.back();
+  }
 }
